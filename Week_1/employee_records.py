6301f4d4153c35b8afe7415031b923bed43b04ee
Justmichae <machariam000@gmail.com>
2024-02-22 07:56:13 +0300

    sixth commit

--- Week_1/employee_records.py
+++ Week_1/employee_records.py
@@ -2,23 +2,25 @@
 #Date: 21/02/2024
 #Name: Michael Macharia
  
-
+#Employee details
 f_name=input("enter first name: ")
 s_name=input("enter second name: ")
 age=input("enter age: ")
 
+#Employee salary and adjustments
 sal=int(input("enter salary: "))
 sal_increase=int(input("enter increment"))
 percentage=(sal_increase / 100)
 sal_increased=(sal * percentage + sal)
 print("salary increased",sal_increased)
 
-
+#Employee bonuses and adjustments
 bonus=int(input("enter bonus "))
 bonus_decreased=int(input("enter bonus decreased"))
 bonus_remained=(bonus - bonus_decreased)
 print("solution of bonus that remained", bonus_remained)
 
+#Total income after adjustments
 total_income=(sal_increased + bonus_remained)
 print("total income after adjustments", total_income)
 
