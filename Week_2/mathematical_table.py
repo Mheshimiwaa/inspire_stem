cd33e9978f33c62b929d4275736db27f187019d3
Justmichae <machariam000@gmail.com>
2024-02-24 02:59:28 +0300

    palindrome,pascals

---
+++ Week_2/mathematical_table.py
@@ -0,0 +1,9 @@
+#Programme for multiplication table
+#Date:22/02/2024
+#Name:Michael Macharia
+
+
+
+for i in range(1,11):
+        for j in range(1,11):
+            print(i,"*",j,"=",i*j,end="\t")            
